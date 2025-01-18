83f8c8d2802ae7891674aa4f810a97bb24a29559
Lucas Dias <diaslucp@gmail.com>
2025-01-18 14:29:28 -0500

    added leafnode and unit tests for it

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,16 +6,17 @@ class HTMLNode():
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
         html_string = ""
         if self.props != None:
             for prop in self.props:
-                html_string += " " + prop + "=\"" + self.props[prop] + '"'
+                html_string += f' {prop}="{self.props[prop]}"'
         return html_string
     def __repr__(self):
         repr_string = f"-HTMLNode Object-\n Tag: {self.tag}\n Value: {self.value}\n Children: {self.children}\n Props:{self.props}\n"
         if self.props != None:
             repr_string +=  f"Props_to_html:{self.props_to_html()}"
         return repr_string
+    
---
+++ src/test_leafnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_children(self):
+        node = LeafNode(value=None, tag=None)
+        self.assertEqual(node.children, None)
+
+    def test_node_no_value_error(self):
+        node = LeafNode(tag='a', value=None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_node_no_tag(self):
+        node = LeafNode(tag=None, value="This is the value")
+        expected = "This is the value"
+        self.assertEqual(node.to_html(), expected)
+    def test_to_html_no_prop(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        expected = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), expected)
+
+    def test_to_html_with_prop(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        expected = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), expected)
+
+if __name__ == "__main__":
+    unittest.main()
