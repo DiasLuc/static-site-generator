975b2a13ea50e952df5794e49720e282bdbe3416
Lucas Dias <diaslucp@gmail.com>
2025-01-21 14:38:40 -0500

    split images and links to node, and unit tests

--- src/markdowntonode.py
+++ src/markdowntonode.py
@@ -1,5 +1,5 @@
 from textnode import TextType, TextNode, Enum
-
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -30,7 +30,55 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
-# node = TextNode("Here is `code` and more `code`", TextType.TEXT)
-# new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+def extract_markdown_images(text):
+    matches = re.findall(r"\!\[(.*?)\]\((.*?)\)",text)
+    return matches
 
-# print(f"New Nodes = {new_nodes}")
+def extract_markdown_links(text):
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)",text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if extract_markdown_images(node.text) == []:
+            new_nodes.append(node)
+            continue #skip to next node!!
+        
+        remaining_text = node.text
+        images = extract_markdown_images(node.text)
+
+        for image in images:
+            alt_text, image_link = image
+
+            sections = remaining_text.split(f"![{alt_text}]({image_link})", 1)
+            if sections[0]: 
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, image_link))
+            remaining_text = sections[1]
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))    
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if extract_markdown_links(node.text) == []:
+            new_nodes.append(node)
+            continue #skip to next node!!
+        
+        remaining_text = node.text
+        links = extract_markdown_links(node.text)
+
+        for link in links:
+            link_text, link_url = link
+
+            sections = remaining_text.split(f"[{link_text}]({link_url})", 1)
+            if sections[0]: 
+                new_nodes.append(TextNode(sections[0],TextType.TEXT))
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            remaining_text = sections[1]
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))    
+    return new_nodes
--- src/test_markdowntonode.py
+++ src/test_markdowntonode.py
@@ -1,6 +1,12 @@
 import unittest
 
-from markdowntonode import *
+from markdowntonode import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link
+)
 from textnode import TextType, TextNode
 
 class TestTextToHTMLNode(unittest.TestCase):
@@ -49,3 +55,106 @@ class TestTextToHTMLNode(unittest.TestCase):
         node = TextNode("missing closing `delimiter", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+    # extract_markdown_images() test
+    def test_extract_image(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        assert extract_markdown_images(text) == [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+    # extract_markdown_links() test
+    def test_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        assert extract_markdown_links(text) == [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+
+    # split_nodes_image() tests
+    def test_split_node_image_single_image(self):
+        test_nodes = [TextNode("Here is an image ![alt](link)", TextType.TEXT)]
+        split_nodes = split_nodes_image(test_nodes)
+        expected = [
+            TextNode("Here is an image ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "link")
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_image_multiple_images(self):
+        test_nodes = [TextNode("Image ![alt1](link1) and ![alt2](link2)", TextType.TEXT)]
+        split_nodes = split_nodes_image(test_nodes)
+        expected = [
+            TextNode("Image ", TextType.TEXT),
+            TextNode("alt1", TextType.IMAGE, "link1"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("alt2", TextType.IMAGE, "link2")
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_image_image_at_start(self):
+        test_nodes = [TextNode("![alt1](link1) is the first thing", TextType.TEXT)]
+        split_nodes = split_nodes_image(test_nodes)
+        expected = [
+            TextNode("alt1", TextType.IMAGE, "link1"),
+            TextNode(" is the first thing", TextType.TEXT),
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_image_image_at_end(self):
+        test_nodes = [TextNode("Image is at the end ![alt](link)", TextType.TEXT)]
+        split_nodes = split_nodes_image(test_nodes)
+        expected = [
+            TextNode("Image is at the end ", TextType.TEXT),
+            TextNode("alt", TextType.IMAGE, "link"),
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_image_no_image(self):
+        test_nodes = [TextNode("No images in this node", TextType.TEXT)]
+        split_nodes = split_nodes_image(test_nodes)
+        expected = [
+            TextNode("No images in this node", TextType.TEXT),
+        ]
+        assert split_nodes == expected
+
+    # split_nodes_link() tests
+    def test_split_node_link_single_link(self):
+        test_nodes = [TextNode("Here is a link [text](link)", TextType.TEXT)]
+        split_nodes = split_nodes_link(test_nodes)
+        expected = [
+            TextNode("Here is a link ", TextType.TEXT),
+            TextNode("text", TextType.LINK, "link")
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_link_multiple_links(self):
+        test_nodes = [TextNode("Link [text1](link1) and [text2](link2)", TextType.TEXT)]
+        split_nodes = split_nodes_link(test_nodes)
+        expected = [
+            TextNode("Link ", TextType.TEXT),
+            TextNode("text1", TextType.LINK, "link1"),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("text2", TextType.LINK, "link2")
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_link_link_at_start(self):
+        test_nodes = [TextNode("[text](link1) is the first thing", TextType.TEXT)]
+        split_nodes = split_nodes_link(test_nodes)
+        expected = [
+            TextNode("text", TextType.LINK, "link1"),
+            TextNode(" is the first thing", TextType.TEXT),
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_link_link_at_end(self):
+        test_nodes = [TextNode("Link is at the end [text](link)", TextType.TEXT)]
+        split_nodes = split_nodes_link(test_nodes)
+        expected = [
+            TextNode("Link is at the end ", TextType.TEXT),
+            TextNode("text", TextType.LINK, "link"),
+        ]
+        assert split_nodes == expected
+
+    def test_split_node_link_no_link(self):
+        test_nodes = [TextNode("No Links in this node", TextType.TEXT)]
+        split_nodes = split_nodes_link(test_nodes)
+        expected = [
+            TextNode("No Links in this node", TextType.TEXT),
+        ]
+        assert split_nodes == expected
